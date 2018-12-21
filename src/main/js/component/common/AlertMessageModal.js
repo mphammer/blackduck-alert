21e82742ce669741d5c0babad5cc7e94af85b801
jrichard <jrichard@synopsys.com>
2018-12-21 12:23:02 -0500

    Fixing formatting

--- src/main/js/component/common/AlertMessageModal.js
+++ src/main/js/component/common/AlertMessageModal.js
@@ -1,44 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {Modal} from 'react-bootstrap';
-import TextInput from "../../field/input/TextInput";
+import { Modal } from 'react-bootstrap';
+import TextInput from '../../field/input/TextInput';
 
 class AlertMessageModal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            message: ''
-        };
-    }
-
     render() {
         return (<Modal show={this.props.showMessageModal} onHide={this.props.handleClose}>
             <Modal.Header closeButton>
                 <Modal.Title>{this.props.modalTitle}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <TextInput id='message' label='' readOnly={true} name='message' value={this.state.message}/>
+                <TextInput id="message" label="" readOnly name="message" value={this.props.message} />
             </Modal.Body>
             <Modal.Footer>
-                <button id="testCancel" type="button" className="btn btn-link" onClick={this.props.handleClose}>Cancel</button>
+                <button id="testCancel" type="button" className="btn btn-link" on Click={this.props.handleClose}>Cancel</button>
             </Modal.Footer>
         </Modal>);
     }
 }
 
 AlertMessageModal.propTypes = {
+    handleClose: PropTypes.func,
     showMessageModal: PropTypes.bool.isRequired,
-    handleClose: PropTypes.func.isRequired,
     modalTitle: PropTypes.string.isRequired,
-    message: PropTypes.string.isRequired,
-    showPropName: PropTypes.string.isRequired
+    message: PropTypes.string.isRequired
 };
 
 AlertMessageModal.defaultProps = {
-    showMessageModal: false,
     handleClose: () => {
-        this.setProperty({showMessageModal: false});
-    },
+        this.setProperty({ showMessageModal: false });
+    }
 };
 
 export default AlertMessageModal;
